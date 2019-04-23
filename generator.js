ddeee7d82fb11079aecd18511077ab8ddb581dca
kmietotwy <kmietotwy@gmail.com>
2019-04-23 09:24:44 -1000

    created function & extra tests

--- generator.js
+++ generator.js
@@ -1,3 +1,15 @@
 module.exports = function(n) {
   // do work here
+  if (typeof n !== 'number') {
+    return false;
+  }
+
+  let scrambled = [0];
+
+  for (let i = 1; i < n; i++) {
+    let randomNumber = Math.floor(Math.random() * (n - 1)) + 1;
+    scrambled.push(randomNumber);
+  }
+
+  return scrambled;
 };
--- test/spec.js
+++ test/spec.js
@@ -2,14 +2,30 @@ var chai = require('chai');
 var expect = chai.expect;
 var generator = require('../generator.js');
 
-describe('Generate 10 scrambled numbers', function () {
-  it('should have length 10 and not be sorted', function () {
+describe('Generate 10 scrambled numbers', function() {
+  it('unique numbers must start at zero', function() {
+    var result = generator(10);
+    expect(result).to.have.length(10);
+    expect(result[0]).to.equal(0);
+  });
+
+  it('should have length 10 and not be sorted', function() {
     var result = generator(10);
     expect(result).to.have.length(10);
     expect(result).to.not.deep.equals([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
 
-  it('should have length 1000 and not be sorted', function () {
+  it('if called with the same input 10, will generate array with same ten numbers but different ordering', function() {
+    var result = generator(10);
+    expect(result).to.have.length(10);
+    expect(result).to.not.deep.equals([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    var resultTwo = generator(10);
+    expect(resultTwo).to.have.length(10);
+    expect(resultTwo).to.not.deep.equals([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    expect(resultTwo).to.not.equal(result);
+  });
+
+  it('should have length 1000 and not be sorted', function() {
     var notExpected = [];
     for (let i = 0; i < 1000; i++) {
       notExpected.push(i);
