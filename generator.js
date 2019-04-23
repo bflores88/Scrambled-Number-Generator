406ab2740a1474d4a600abea26258d46fbabf2ec
kmietotwy <kmietotwy@gmail.com>
2019-04-23 09:29:16 -1000

    format doc

--- generator.js
+++ generator.js
@@ -1,12 +1,12 @@
 module.exports = function(n) {
   // do work here
-  if(typeof n !== 'number'){
+  if (typeof n !== 'number') {
     return false;
-  };
+  }
 
   let scrambled = [];
 
-  for(let i=0; i<n; i++){
+  for (let i = 0; i < n; i++) {
     let randomNumber = Math.floor(Math.random() * n);
     scrambled.push(randomNumber);
   }
