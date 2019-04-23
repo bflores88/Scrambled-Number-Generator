847868cdd5c67b4003587de1cd2c37e2463f15f3
kmietotwy <kmietotwy@gmail.com>
2019-04-23 09:28:47 -1000

    accidentally removed closing parentheses

--- generator.js
+++ generator.js
@@ -1,13 +1,13 @@
 module.exports = function(n) {
   // do work here
-  if (typeof n !== 'number') {
+  if(typeof n !== 'number'){
     return false;
-  }
+  };
 
-  let scrambled = [0];
+  let scrambled = [];
 
-  for (let i = 0; i < n; i++) {
-    let randomNumber = Math.floor(Math.random() * n;
+  for(let i=0; i<n; i++){
+    let randomNumber = Math.floor(Math.random() * n);
     scrambled.push(randomNumber);
   }
 
